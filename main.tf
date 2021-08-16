c8981ca1937678540fba075494e792060ba453ca
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2021-08-16 09:43:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "1.6.0"
+    }
+  }
+}
+
 provider "azuread" {
-  version = "~> 2.0.0"
+  # Configuration options
 }
 
 provider "azurerm" {
